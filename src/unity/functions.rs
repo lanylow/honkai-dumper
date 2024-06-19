0b5c61fb125cf0550439bf30a8fe3db51eebe00f
lanylow <lanylow@protonmail.com>
2024-06-19 14:41:03 +0200

    Updated for Honkai: Star Rail 2.3.0.

--- src/unity/functions.rs
+++ src/unity/functions.rs
@@ -17,15 +17,15 @@ pub struct Il2CppFunctions {
 impl Il2CppFunctions {
   pub fn new(base: usize) -> Self {
     Il2CppFunctions {
-      il2cpp_domain_get: get_method_ptr(base + 0x1cf2e48),
-      il2cpp_domain_get_assemblies: get_method_ptr(base + 0x1cf2e58),
-      il2cpp_assembly_get_image: get_method_ptr(base + 0x1cf2d10),
-      il2cpp_image_get_class_count: get_method_ptr(base + 0x1cf3178),
-      il2cpp_image_get_class: get_method_ptr(base + 0x1cf3180),
-      il2cpp_class_get_methods: get_method_ptr(base + 0x1cf2d78),
-      il2cpp_class_get_name: get_method_ptr(base + 0x1cf2d88),
-      il2cpp_class_get_namespace: get_method_ptr(base + 0x1cf2d98),
-      il2cpp_method_get_name: get_method_ptr(base + 0x1cf2fd8)
+      il2cpp_domain_get: get_method_ptr(base + 0x1d3ddc0),
+      il2cpp_domain_get_assemblies: get_method_ptr(base + 0x1d3ddd0),
+      il2cpp_assembly_get_image: get_method_ptr(base + 0x1d3dc88),
+      il2cpp_image_get_class_count: get_method_ptr(base + 0x1d3e680),
+      il2cpp_image_get_class: get_method_ptr(base + 0x1d3e6a0),
+      il2cpp_class_get_methods: get_method_ptr(base + 0x1d3dcf0),
+      il2cpp_class_get_name: get_method_ptr(base + 0x1d3dd00),
+      il2cpp_class_get_namespace: get_method_ptr(base + 0x1d3dd10),
+      il2cpp_method_get_name: get_method_ptr(base + 0x1d3e118)
     }
   }
 }

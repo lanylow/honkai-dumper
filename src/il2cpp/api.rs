0badcf52056bde50b060d93ceed3d6487e93b95c
lanylow <lanylow@protonmail.com>
2025-01-23 12:45:08 +0100

    Improvements to error handling.

--- src/il2cpp/api.rs
+++ src/il2cpp/api.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, ffi::{c_char, c_void, CStr}, path::PathBuf};
+use std::{error::Error, ffi::{c_void, CStr}, path::PathBuf, str::Utf8Error};
 use thiserror::Error;
 use super::{functions::Il2CppFunctions, module::{Module, ModuleError}, types::MethodInfo};
 
@@ -6,13 +6,17 @@ use super::{functions::Il2CppFunctions, module::{Module, ModuleError}, types::Me
 pub enum Il2CppError {
   #[error(transparent)]
   Module(#[from] ModuleError),
+  #[error(transparent)]
+  Utf8(#[from] Utf8Error),
 
   #[error("file not found {0}")]
   FileNotFound(&'static str),
   #[error("function not found {0}")]
   FunctionNotFound(&'static str),
   #[error("root path not found")]
-  RootNotFound
+  RootNotFound,
+  #[error("function returned null {0}")]
+  ReturnedNull(&'static str)
 }
 
 pub struct Il2CppApi {
@@ -45,46 +49,59 @@ impl Il2CppApi {
     })
   }
 
-  fn to_string(native: *const c_char) -> String {
-    let str = unsafe { CStr::from_ptr(native) };
-    str.to_str().unwrap().to_string()
-  }
-
   pub fn domain_get(&self) -> Result<*const c_void, Il2CppError> {
     let function = &self
       .functions
-      .clone()
       .il2cpp_domain_get
+      .as_ref()
       .ok_or(Il2CppError::FunctionNotFound("il2cpp_domain_get"))?;
 
-    Ok(function())
+    let domain = function();
+
+    if domain.is_null() {
+      return Err(Il2CppError::ReturnedNull("il2cpp_domain_get"));
+    }
+
+    Ok(domain)
   }
 
   pub fn domain_get_assemblies(&self, domain: *const c_void, size: *const usize) -> Result<*const *const c_void, Il2CppError> {
     let function = &self
       .functions
-      .clone()
       .il2cpp_domain_get_assemblies
+      .as_ref()
       .ok_or(Il2CppError::FunctionNotFound("il2cpp_domain_get_assemblies"))?;
 
-    Ok(function(domain, size))
+    let assemblies = function(domain, size);
+
+    if assemblies.is_null() {
+      return Err(Il2CppError::ReturnedNull("il2cpp_domain_get_assemblies"));
+    }
+
+    Ok(assemblies)
   }
 
   pub fn assembly_get_image(&self, assembly: *const c_void) -> Result<*const c_void, Il2CppError> {
     let function = &self
       .functions
-      .clone()
       .il2cpp_assembly_get_image
+      .as_ref()
       .ok_or(Il2CppError::FunctionNotFound("il2cpp_assembly_get_image"))?;
+      
+    let image = function(assembly);
 
-    Ok(function(assembly))
+    if image.is_null() {
+      return Err(Il2CppError::ReturnedNull("il2cpp_assembly_get_image"));
+    }
+
+    Ok(image)
   }
 
   pub fn image_get_class_count(&self, image: *const c_void) -> Result<usize, Il2CppError> {
     let function = &self
       .functions
-      .clone()
       .il2cpp_image_get_class_count
+      .as_ref()
       .ok_or(Il2CppError::FunctionNotFound("il2cpp_image_get_class_count"))?;
 
     Ok(function(image))
@@ -93,57 +110,83 @@ impl Il2CppApi {
   pub fn image_get_class(&self, image: *const c_void, index: usize) -> Result<*const c_void, Il2CppError> {
     let function = &self
       .functions
-      .clone()
       .il2cpp_image_get_class
+      .as_ref()
       .ok_or(Il2CppError::FunctionNotFound("il2cpp_image_get_class"))?;
 
-    Ok(function(image, index))
+    let class = function(image, index);
+
+    if class.is_null() {
+      return Err(Il2CppError::ReturnedNull("il2cpp_image_get_class"));
+    }
+
+    Ok(class)
   }
 
   pub fn class_get_methods(&self, class: *const c_void, iter: *const *const c_void) -> Result<Option<*const MethodInfo>, Il2CppError> {
     let function = &self
       .functions
-      .clone()
       .il2cpp_class_get_methods
+      .as_ref()
       .ok_or(Il2CppError::FunctionNotFound("il2cpp_class_get_methods"))?;
 
     let result = function(class, iter);
 
-    Ok(if !result.is_null() { 
-      Some(result)
-    } else {
-      None
-    })
+    Ok(if result.is_null() { None } else { Some(result) })
   }
 
   pub fn class_get_name(&self, class: *const c_void) -> Result<String, Il2CppError> {
     let function = &self
       .functions
-      .clone()
       .il2cpp_class_get_name
+      .as_ref()
       .ok_or(Il2CppError::FunctionNotFound("il2cpp_class_get_name"))?;
 
-    Ok(Self::to_string(function(class)))
+    let name_c = function(class);
+
+    if name_c.is_null() {
+      return Err(Il2CppError::ReturnedNull("il2cpp_class_get_name"));
+    }
+
+    let name = unsafe { CStr::from_ptr(name_c) };
+
+    Ok(name.to_str()?.to_string())
   }
 
   pub fn class_get_namespace(&self, class: *const c_void) -> Result<String, Il2CppError> {
     let function = &self
       .functions
-      .clone()
       .il2cpp_class_get_namespace
+      .as_ref()
       .ok_or(Il2CppError::FunctionNotFound("il2cpp_class_get_namespace"))?;
 
-    Ok(Self::to_string(function(class)))
+    let name_c = function(class);
+
+    if name_c.is_null() {
+      return Err(Il2CppError::ReturnedNull("il2cpp_class_get_namespace"));
+    }
+
+    let name = unsafe { CStr::from_ptr(name_c) };
+
+    Ok(name.to_str()?.to_string())
   }
 
   pub fn method_get_name(&self, method: *const MethodInfo) -> Result<String, Il2CppError> {
     let function = &self
       .functions
-      .clone()
       .il2cpp_method_get_name
+      .as_ref()
       .ok_or(Il2CppError::FunctionNotFound("il2cpp_method_get_name"))?;
 
-    Ok(Self::to_string(function(method)))
+    let name_c = function(method);
+
+    if name_c.is_null() {
+      return Err(Il2CppError::ReturnedNull("il2cpp_method_get_name"));
+    }
+
+    let name = unsafe { CStr::from_ptr(name_c) };
+
+    Ok(name.to_str()?.to_string())
   }
 }
 
